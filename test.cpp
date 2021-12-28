89b2b4131f43848d6b9ac71607ff58402e644ce8
KiUkKim <zidh1@korea.ac.kr>
2021-12-28 16:43:44 +0900

    algospot-JongManBook Problem ID :  FESTIVAL

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    fastio;
+
+    double a;
+    double b = 7;
+    int c = 4;
+    a = b/c;
+
+    cout << a << '\n';
+
+    return 0;
+}
